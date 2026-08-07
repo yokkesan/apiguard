fc9a8d9bef847dc209b17814a1a553a2ba6979cb
yokkesan <lc.yteshima@gmail.com>
2026-08-07 10:36:36 +0900

    add Gin route and middleware detection

--- internal/analyzer/go_analyzer.go
+++ internal/analyzer/go_analyzer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go/parser"
 	"go/token"
+	"strings"
 )
 
 type GoAnalyzer struct{}
@@ -31,8 +32,28 @@ func (g GoAnalyzer) Analyze(path string) []Issue {
 	issues := DetectSecret(node, path, fs)
 
 	sqlIssues := DetectSQLInjection(node, path, fs)
-
 	issues = append(issues, sqlIssues...)
+
+	routes := DetectGinRoutes(node, fs)
+
+	authIssues := DetectMissingAuth(routes, path)
+	issues = append(issues, authIssues...)
+
+	for _, route := range routes {
+		message := route.Method + " " + route.Path + " -> " + route.Handler
+
+		if len(route.Middlewares) > 0 {
+			message += " middleware: " + strings.Join(route.Middlewares, ", ")
+		}
+
+		issues = append(issues, Issue{
+			Code:    "INFO-ROUTE-001",
+			Level:   "INFO",
+			Message: message,
+			File:    path,
+			Line:    route.Line,
+		})
+	}
 
 	return issues
 }
--- sample.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	id := "1"
-
-	query := "SELECT * FROM users WHERE id=" + id
-
-	println(query)
-}
